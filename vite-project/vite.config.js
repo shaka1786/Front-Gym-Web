470a65ac124a4c9c0ac47f93f772b8013f047f7e
Samuel Monsalve <smh1786@gmail.com>
2025-10-31 19:02:05 -0500

    31-10-2025 mod vite.config para dominio

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // escucha en 0.0.0.0
+    port: 5173, // opcional, pero así te aseguras
+    allowedHosts: [
+      "visualstudiocode1.smh1786.space", // para poder publicarlo con dominio
+    ],
+  },
+});
